46c19e7cc9a2b8aca0b3653484fbdf95f3f0e6c3
RlEHVL <165342717+RlEHVL@users.noreply.github.com>
2024-12-07 02:40:19 +0900

    Create CObj.c

---
+++ shooter/CObj.c
@@ -0,0 +1,28 @@
+#include "CObj.h"
+#include <stdlib.h>
+
+CObj* CObjFactory_CreateCBullet(float x, float y, struct CBridge* bridge) {
+    CObj* obj = (CObj*)malloc(sizeof(CObj));
+    obj->x = x;
+    obj->y = y;
+    obj->bridge = bridge;
+    obj->active = true;
+    return obj;
+}
+
+void CObj_SetPos(CObj* pObj, float x, float y) {
+    pObj->x = x;
+    pObj->y = y;
+}
+
+void CObj_Initialize(CObj* pObj) {
+    pObj->active = true;
+}
+
+bool CObj_IsActive(CObj* pObj) {
+    return pObj->active;
+}
+
+void CObj_SetActive(CObj* pObj, bool active) {
+    pObj->active = active;
+}
