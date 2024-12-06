77d9a232e15631554ed403687eeafd8b2feba889
RlEHVL <165342717+RlEHVL@users.noreply.github.com>
2024-12-07 02:41:16 +0900

    Create CThrustBulletA.h

---
+++ shooter/CThrustBulletA.h
@@ -0,0 +1,9 @@
+#ifndef CTHRUSTBULLETA_H
+#define CTHRUSTBULLETA_H
+
+#include "CBridge.h"
+
+void CThrustBulletA_SetTarget(CBridge* pBridge, struct CObj* pTarget);
+void CThrustBulletA_SetTime(CBridge* pBridge, int time);
+
+#endif // CTHRUSTBULLETA_H
