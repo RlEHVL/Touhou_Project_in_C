f26a66468c8aeeb4636eb05109b815ef4d3b9ddf
Herta <165342717+RlEHVL@users.noreply.github.com>
2024-12-11 19:17:55 +0900

    Update CThrustBulletA.h

--- shooter/CThrustBulletA.h
+++ shooter/CThrustBulletA.h
@@ -1,6 +1,12 @@
 #ifndef CTHRUSTBULLETA_H
 #define CTHRUSTBULLETA_H
 
+/*
+    *
+    * 제작:동필
+    * 도움이:지피티
+*/
+
 #include "CBridge.h"
 
 void CThrustBulletA_SetTarget(CBridge* pBridge, struct CObj* pTarget);
